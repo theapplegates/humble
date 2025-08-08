ce59b83436eb33c69543e0bc0ea85d6f2af87af8
Paul <paul@theapplegates.com>
2025-08-07 22:19:36 -0400

    Test

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,6 +20,6 @@ export default defineConfig({
     skipInline: false,
     drafts: true
   },
-  site: 'https://yourwebsite.com',
+  site: 'https://humble.paulapplegate.com',
   integrations: [sitemap(), mdx()]
 });
